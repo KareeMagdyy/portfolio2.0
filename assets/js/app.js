6da2492cd4757b66fcf8df8394644494a47aeba4
KareeMagdyy <karim67899@gmail.com>
2022-01-06 21:34:54 +0200

    adding Animation to home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -116,3 +116,43 @@ const swiper = new Swiper(".testimonial__container", {
     },
   },
 });
+
+/* ====== GSAP Animations ======= */
+
+gsap.from(".home__img", { opacity: 0, duration: 2, delay: 0.5, x: 60 });
+gsap.from(".home__data", { opacity: 0, duration: 2, delay: 0.8, y: 25 });
+gsap.from(".home__greeting, .home__name, .home__profession, .home__button", {
+  opacity: 0,
+  duration: 2,
+  delay: 1,
+  y: 25,
+  ease: "expo.out",
+  stagger: 0.2,
+});
+
+gsap.from(".nav__logo, .nav__toggle", {
+  opacity: 0,
+  duration: 2,
+  delay: 1.5,
+  y: 25,
+  ease: "expo.out",
+  stagger: 0.2,
+});
+
+gsap.from(".nav__item", {
+  opacity: 0,
+  duration: 2,
+  delay: 1.8,
+  y: 25,
+  ease: "expo.out",
+  stagger: 0.2,
+});
+
+gsap.from(".home__social-icon", {
+  opacity: 0,
+  duration: 2,
+  delay: 2.3,
+  y: 25,
+  ease: "expo.out",
+  stagger: 0.2,
+});
